55a4465c515eefb17535c8aa676aeffe05b47791
Mudada <mael.nicolas77@gmail.com>
2024-05-22 18:36:59 +0200

    generate shity 4x4 dirt grid + yoink assets

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,31 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, render::render_resource::ShaderType};
 
-fn hello_world() {
-    println!("hello world");
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>
+    ) {
+
+    commands.spawn(Camera2dBundle::default());
+
+    let tile_row: [i32; 4] = [0, 0, 0, 0];
+    let tile_map: [[i32; 4]; 4] = [tile_row, tile_row, tile_row, tile_row];
+
+    for (i, _) in tile_map.iter().enumerate() {
+        for (j, _) in tile_map[i].iter().enumerate() {
+            let x_pos = ((i as f32) * 60.0) + ((j as f32) * 60.0);
+            let y_pos = ((i as f32) * 30.0) - ((j as f32) * 30.0);
+            commands.spawn(SpriteBundle {
+                texture: asset_server.load("tiles/dirt.png"),
+                transform: Transform::from_xyz(x_pos, y_pos, 0.0),
+                ..default()
+            });
+        }
+    }
 }
 
 fn main() {
     App::new()
-        .add_systems(Update, hello_world)
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
         .run();
 }
